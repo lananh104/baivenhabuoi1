05487775e1d5606203497c0f4f684e856cc91a43
lananh104 <lananh.0312.1999@gmail.com>
2020-04-19 13:14:03 +0700

    baivenhabuoi1

---
+++ baitapvenha_buoi1.py
@@ -0,0 +1,101 @@
+#giải thích các hàm trong thư viện math
+# import math
+# in ra số pi
+# print(math.pi)
+# #in ra số e
+# print(math.e)
+# #in ra số e và lấy 3 chữ số sau dấu phẩy
+# print(round(math.e,3))
+# #in ra số lớn nhất mà nhỏ hơn số đã cho
+# print(math.floor(-5.4))
+# #in ra số nguyên nhỏ nhất lớn hơn số đã cho
+# print(math.ceil(4.9))
+# #in ra phần nguyên
+# print(math.trunc(-1.5))
+# print(math.trunc(2.9))
+# #in ra giai thừa của 1 số
+# print(math.factorial(6))
+# #in ra ước chung lớn nhất
+# print(math.gcd(72,9))
+##lấy giá trị thuyệt đối
+#abs(x)
+
+# bai1
+# print("Twinkle, twinkle, little start,")
+# print("        How I wonder what you are! ")
+# print("                Up above the world so hight,")
+# print("                Like a diamond in the sky.")
+# print("Twinkle, twinkle, little start,")
+# print("        How I wonder what you are")
+# bai 3
+# import datetime
+# date_time=datetime.datetime.now()
+# print("Current date and time:")
+# print(date_time)
+# bai 4
+# import math
+# r=float(input())
+# area=r**2*math.pi
+# print(round(area,2))
+# bai 5
+# print(" First name: ")
+# first_name=input()
+# print(" Last name: ")
+# last_name=input()
+# print(last_name + " " + first_name)
+# print("{} {}".format(last_name,first_name))
+# #bai 7
+# a="baivenha.py"
+# b=a[-2:None]
+# print(b)b
+#bai8
+# a=("Red","Green","White","Black")b
+# b=a[0],a[3]
+# print(b)
+#bai10
+# a=int(input())
+# b=a+(a*10+a)+(a*100+a*10+a)
+# print(b)
+#bai12
+#bai13
+# a='''
+# Đất là nơi Chim vềV
+# Nước là nơi Rồng ở
+# LẠc Long Quân và Âu Cơ
+# Đẻ ra đồng bào ta trong bọc trứng
+# '''
+# print(a)
+#bai15
+# import math
+# r=6
+# V=math.pi*4/3*r**3
+# print(round(V,3))
+#bai16
+# import math
+# a=float(input())
+# b=17-a
+# if a < 17:
+#    print(b)
+# else:
+#     print(2*abs(b))
+#bai18
+
+# a=float(input())
+# b=float(input())
+# c=float(input())
+# if a!=b and b!=c:
+#    S=a+b+c
+#    print(S)
+# if a==b and b==c :
+#    S=3*a
+#    print(S)
+#bai25a
+# a=[1,2,6,4,"x y z"]
+# b="x y z" in a
+# print(b)
+
+
+
+
+
+
